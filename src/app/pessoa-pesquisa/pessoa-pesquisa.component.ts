cb680f5865e6d72ea48e2e7ca37c9ee253255374
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-08-26 22:09:22 -0300

    Desafio: usando pipes.

--- src/app/pessoa-pesquisa/pessoa-pesquisa.component.ts
+++ src/app/pessoa-pesquisa/pessoa-pesquisa.component.ts
@@ -7,15 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PessoaPesquisaComponent {
   pessoas = [
-    { status: true, nome: 'Elaine Souza', dataNascimento: '10/05/1988', cidade: 'Mossoró', estado: 'RN'},
-    { status: true, nome: 'Helena Lopes', dataNascimento: '13/05/2009', cidade: 'Mossoró', estado: 'RN'},
-    { status: true, nome: 'Anderson Benigno Lopes', dataNascimento: '13/12/1981', cidade: 'Fortaleza', estado: 'CE'},
-    { status: false, nome: 'Eliene Lopes', dataNascimento: '10/01/1952', cidade: 'Mossoró', estado: 'RN'},
-    { status: false, nome: 'Hélio Lopes', dataNascimento: '10/10/1977', cidade: 'Mossoró', estado: 'RN'},
-    { status: true, nome: 'Ramísia Souza', dataNascimento: '16/06/2000', cidade: 'Mossoró', estado: 'RN'},
-    { status: true, nome: 'Aline Mayara', dataNascimento: '28/02/1995', cidade: 'Aracati', estado: 'CE'},
-    { status: true, nome: 'Francisco Deassis Lopes', dataNascimento: '10/05/1948', cidade: 'Almino Afonso', estado: 'RN'},
-    { status: false, nome: 'Maria de Lourdes', dataNascimento: '10/05/1958', cidade: 'Mossoró', estado: 'RN'}
+    { status: true, nome: 'Elaine Souza', dataNascimento: new Date(1988, 4, 10), cidade: 'Mossoró', estado: 'RN'},
+    { status: true, nome: 'Helena Lopes', dataNascimento: new Date(2009, 4, 13), cidade: 'Mossoró', estado: 'RN'},
+    { status: true, nome: 'Anderson Benigno Lopes', dataNascimento: new Date(1981, 11, 13), cidade: 'Fortaleza', estado: 'CE'},
+    { status: false, nome: 'Eliene Lopes', dataNascimento: new Date(1957, 12, 10), cidade: 'Mossoró', estado: 'RN'},
+    { status: false, nome: 'Hélio Lopes', dataNascimento: new Date(1977, 10, 10), cidade: 'Mossoró', estado: 'RN'},
+    { status: true, nome: 'Ramísia Souza', dataNascimento: new Date(2000, 5, 16), cidade: 'Mossoró', estado: 'RN'},
+    { status: true, nome: 'Aline Mayara', dataNascimento: new Date(1995, 1, 28), cidade: 'Aracati', estado: 'CE'},
+    { status: true, nome: 'Francisco Deassis Lopes', dataNascimento: new Date(1948, 9, 20), cidade: 'Almino Afonso', estado: 'RN'},
+    { status: false, nome: 'Maria de Lourdes', dataNascimento: new Date(1964, 7, 23), cidade: 'Aracati', estado: 'CE'}
   ];
 
 }
