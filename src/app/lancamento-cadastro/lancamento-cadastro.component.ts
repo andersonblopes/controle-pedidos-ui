40f90d2accea4c0ac72cae7df5678d06e3ad9c6c
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-08-27 22:34:47 -0300

    Adicionando seletor de data (componente Calendar).

--- src/app/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -6,5 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lancamento-cadastro.component.css']
 })
 export class LancamentoCadastroComponent {
+  pt: any;
+  ngOnInit() {
+    this.pt = {
+      firstDayOfWeek: 0,
+      dayNames: ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"],
+      dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+      dayNamesMin: ["Dm", "Sg", "Tr", "Qr", "Qn", "Sx", "Sb"],
+      monthNames: ["Janeiro", "Fevereiro", "Mar;o", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+      monthNamesShort: ["Jan", "Fev", "Mar", "Acr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
+    };
+  }
 
 }
