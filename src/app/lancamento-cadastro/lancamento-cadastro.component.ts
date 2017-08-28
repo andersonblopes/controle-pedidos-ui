549cf27e8c046371a14fe061e4e7584791bcd495
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-08-27 21:45:05 -0300

    Criando o protótipo do formulário de cadastro de lançamentos.

---
+++ src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent {
+
+}
