4e344c291b8bec6def7b16b5f7f3a7ef3448a8eb
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-08-27 22:46:51 -0300

    Traduzindo componente calendar.

--- src/app/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -5,16 +5,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './lancamento-cadastro.component.html',
   styleUrls: ['./lancamento-cadastro.component.css']
 })
-export class LancamentoCadastroComponent {
+export class LancamentoCadastroComponent implements OnInit {
   pt: any;
+
+  constructor() { }
+
   ngOnInit() {
     this.pt = {
       firstDayOfWeek: 0,
-      dayNames: ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"],
-      dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
-      dayNamesMin: ["Dm", "Sg", "Tr", "Qr", "Qn", "Sx", "Sb"],
-      monthNames: ["Janeiro", "Fevereiro", "Mar;o", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
-      monthNamesShort: ["Jan", "Fev", "Mar", "Acr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
+      dayNames: [`Domingo`, `Segunda-feira`, `Terça-feira`, `Quarta-feira`, `Quinta-feira`, `Sexta-feira`, `Sábado`],
+      dayNamesShort: [`Dom`, `Seg`, `Ter`, `Qua`, `Qui`, `Sex`, `Sáb`],
+      dayNamesMin: [`Dm`, `Sg`, `Tr`, `Qr`, `Qn`, `Sx`, `Sb`],
+      monthNames: [`Janeiro`, `Fevereiro`, `Mar;o`, `Abril`, `Maio`, `Junho`, `Julho`, `Agosto`, `Setembro`, `Outubro`,
+        `Novembro`, `Dezembro`],
+      monthNamesShort: [`Jan`, `Fev`, `Mar`, `Acr`, `Mai`, `Jun`, `Jul`, `Ago`, `Set`, `Out`, `Nov`, `Dez`]
     };
   }
 
